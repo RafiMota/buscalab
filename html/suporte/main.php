fe906fd43c63080e9413aad0100cd50bfd17b1f0
Kauã S. Maia <116105882+Hactchubas@users.noreply.github.com>
2023-07-04 05:17:47 -0300

    Corrigido em main.php: link dos labs, notificação, num de problemas e labs com problemas

--- html/suporte/main.php
+++ html/suporte/main.php
@@ -18,17 +18,46 @@
     // }
 
     // Consulta SQL
-    $sql = "SELECT id, laboratório, categoria, software, equipamento, problema, outro_problema, mesa, situação FROM problemas";
-    $sql2 = "SELECT id, laboratório, categoria, software, equipamento, problema, outro_problema, mesa, situação FROM problemas ORDER BY laboratório ASC";
+    
+    $sql = "SELECT id, laboratório, categoria, software, equipamento, problema, outro_problema, mesa, situação 
+            FROM problemas
+            ORDER BY laboratório ASC";
 
-    $result = mysqli_query($conn, $sql2);
+    $slq_labs_c_problemas = "SELECT DISTINCT laboratório
+                             FROM problemas
+                             ORDER BY laboratório ASC";
+
+    $sql_num_report_lab = "SELECT Count(laboratório) AS num, laboratório AS lab
+                           FROM problemas 
+                           GROUP BY laboratório
+                           ORDER BY laboratório ASC";
+
+    $result = mysqli_query($conn, $sql);
     $row_count = mysqli_num_rows($result);
     if (!$result) {
-        echo "Nao foi possivel executar a consulta ($sql2) no banco de dados: " . mysqli_error($conn);
+        echo "Nao foi possivel executar a consulta (".$sql.") no banco de dados: " . mysqli_error($conn);
         exit;
     }
 
     if (mysqli_num_rows($result) == 0) {
+        echo "Nao foram encontradas linhas, nada para mostrar<br><br>";
+    }
+    $result_labs_c_problemas = mysqli_query($conn, $slq_labs_c_problemas);    
+    if (!$result_labs_c_problemas) {
+        echo "Nao foi possivel executar a consulta (".$slq_labs_c_problemas.") no banco de dados: " . mysqli_error($conn);
+        exit;
+    }
+
+    if (mysqli_num_rows($result_labs_c_problemas) == 0) {
+        echo "Nao foram encontradas linhas, nada para mostrar<br><br>";
+    }
+    $result_num_report_lab = mysqli_query($conn, $sql_num_report_lab);    
+    if (!$result_num_report_lab) {
+        echo "Nao foi possivel executar a consulta (".$sql_num_report_lab.") no banco de dados: " . mysqli_error($conn);
+        exit;
+    }
+
+    if (mysqli_num_rows($result_num_report_lab) == 0) {
         echo "Nao foram encontradas linhas, nada para mostrar<br><br>";
     }
 
@@ -63,24 +92,56 @@
         </section>
     </header>
     <section class="flex bg-slate-100 p-8">
-        <nav class="flex flex-col gap-3 w-1/6 p-4 bg-slate-50 rounded-lg shadow-lg">
+        <nav class="flex flex-col gap-3 w-1/4 p-4 bg-slate-50 rounded-lg shadow-lg">
             <h3 class="text-lg font-semibold">Laboratórios</h3>
-            <a class="transition-all hover:font-semibold" href="">Laboratório 1</a>
-            <a class="transition-all hover:font-semibold" href="">Laboratório 2</a>
-            <a class="transition-all hover:font-semibold" href="">Laboratório 3</a>
-            <a class="transition-all hover:font-semibold" href="">Laboratório 4</a>
-            <a class="transition-all hover:font-semibold" href="">Laboratório 5</a>
-            <a class="transition-all hover:font-semibold" href="">Laboratório 6</a>
+            <?php
+                $row = mysqli_fetch_all($result_num_report_lab);
+                $aux = 0; 
+                
+                for($i = 1; $i <7; $i++){
+                    echo '<a class="transition-all hover:font-semibold flex flex-row flex-nowrap gap-4" href="">
+                            <p>Laboratório '.$i.'</p>';  
+                            
+                    if($row[$aux][1]==$i){
+                        echo "<div class='bg-orange-400 w-4 rounded-md text-center'>".$row[$aux][0]."</div>";
+                        $aux++;
+                    }
+                    echo '</a>';
+                } 
+                // while($row = mysqli_fetch_all($result_num_report_lab)){  
+                //     print_r($row);                  
+                //     echo '<a class="transition-all hover:font-semibold flex flex-row flex-nowrap gap-4" href="">
+                //             <p>Laboratório '.$row["lab"].'</p> 
+                //             <div class="bg-orange-400 w-4 rounded-md text-center">'.$row["num"].'</div>
+                //             </a>';                    
+                // }
+            ?>
         </nav>
 
         <main class="flex flex-col p-4 gap-8 w-full ">
             <section>
                 <div class="flex flex-row justify-between">
                     <h2 class="text-2xl font-semibold">Reportes</h2>
-                    <h3 class="text-2xl">Problemas: <span class="font-semibold">3</span></h3>
+                    <h3 class="text-2xl">Problemas: <span class="font-semibold"><?php echo $row_count; ?></span></h3>
                 </div>
                 <div>
-                    <p class="text-lg">Laboratórios com problemas: <span class="font-semibold">2, 3</span></p>
+                    <p class="text-lg">Laboratórios com problemas:
+                         <span class="font-semibold">
+                            <?php 
+                            $num_result_labs_c_problemas = mysqli_num_rows($result_labs_c_problemas);
+                            $aux = 0;
+                            while ($row = mysqli_fetch_assoc($result_labs_c_problemas)) { 
+                                $aux++;
+                                echo $row["laboratório"];
+                                if($aux < $num_result_labs_c_problemas){
+                                    echo ", ";
+                                }
+                                
+                                // print_r($row);
+                            }                            
+                            ?>
+                         </span>
+                        </p>
                 </div>
             </section>
 
@@ -91,16 +152,8 @@
             // Enquanto uma linha de dados existir, coloca esta linha em $row como uma matriz associativa e mostra os dados na página
             $first = True;
             $n_rows = 1;
-            while ($row = mysqli_fetch_assoc($result)) {
+            while ($row = mysqli_fetch_assoc($result)) {     
                 
-                // print_r ($row);
-                // echo '<div class="agrupamento> laboratório: ' . $row["laboratório"] . '<br>';
-                // echo 'categoria: ' . $row["categoria"] . '<br>';
-                // echo 'software:' . $row["software"] . '<br>';
-                // echo 'equipamento:' . $row["equipamento"] . '<br>';
-                // echo 'problema: ' . $row["problema"] . '<br>';
-                // echo 'outro problema: ' . $row["outro_problema"] . '<br>';
-                // echo 'mesa: ' . $row["mesa"] . '<br>';
                 
                 $situacao = $row["situação"];
                     $id = $row["id"];
@@ -122,7 +175,7 @@
                             <h3 class="font-semibold text-xl p-4 transition-all">
                             Laboratório '. $row["laboratório"]. '
                             </h3>
-                            <article class="flex flex-row gap-4" >'; 
+                            <article class="flex flex-row flex-wrap gap-4" >'; 
                     $first = false;
                 }
                 
@@ -138,13 +191,13 @@
                             <h3 class="font-semibold text-xl p-4 transition-all">
                             Laboratório ' . $row["laboratório"] . '
                             </h3>
-                            <article class="flex flex-row gap-4" >';  
+                            <article class="flex flex-row flex-wrap gap-4">';  
                                                               
                 }                    
                 
                 echo '          
                 
-                                <div class="cursor-pointer bg-slate-50 rounded-xl w-1/5 flex flex-col shadow-xl hover:shadow-2xl transition-all duration-300" onclick="concluiTarefa()">
+                                <div class="cursor-pointer bg-slate-50 rounded-xl w-1/5 shrink-0 shadow-xl hover:shadow-2xl transition-all duration-300"  onclick="concluiTarefa()">
                                     <div class="p-4">
                                         <p class="w-full">Mesa:</p>
                                         <span>'. $row["mesa"] .'</span>
@@ -168,20 +221,7 @@
                         </article> </section>';
                 } 
                         
-                $n_rows++;
-                // Parte relacionada ao pagamento e validade da reserva 
-                // $situacao = $row["situação"];
-                // $id = $row["id"];
-                // if($situacao == 1){
-                //     echo 'situação: a fazer';
-                //     echo "<p><a href='update.php?id=$id'>Reporte em andamento</a><p></div><br>";
-                // }else if($situacao == 2){
-                //     echo 'situação: em andamento';
-                //     echo "<p><a href='update.php?id=$id'>Reporte concluído</a><p></div><br>";
-                // }else if($situacao == 3){
-                //     echo 'situação: feito';
-                //     echo "<p><a href='remover.php?id=$id'>Remover reporte</a><p></div><br>";
-                // }
+                $n_rows++;        
                 
             }
 
